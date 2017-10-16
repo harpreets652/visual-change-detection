e8acf7e43b37680be821836ebe92d1027c99ee2d
harpreetsingh <harpreets@nevada.unr.edu>
2017-10-15 21:29:55 -0700

    SURF feature detection

--- change-detection/src/main.cpp
+++ change-detection/src/main.cpp
@@ -1,26 +1,54 @@
 #include <iostream>
+#include <vector>
 #include <opencv2/opencv.hpp>
-
+#include <opencv2/xfeatures2d.hpp>
 
 void showImage(std::string &imageName);
 
-int main() {
+void showImage(cv::Mat &image);
 
-    std::string imageName = "../resources/chessboard-texture.png";
-    showImage(imageName);
+void featureDetectorSURF(std::string &imageName);
+
+int main() {
+    std::string imageName = "../resources/chessboard.png";
+    featureDetectorSURF(imageName);
 
     return 0;
 }
 
-void showImage(std::string &imageName){
+void featureDetectorSURF(std::string &imageName) {
+    cv::Mat inputImage = cv::imread(imageName, cv::IMREAD_GRAYSCALE);
+
+    if (!inputImage.data) {
+        std::cerr << "Unable to open input image " << imageName << std::endl;
+        return;
+    }
+
+    cv::Ptr<cv::xfeatures2d::SURF> detector = cv::xfeatures2d::SURF::create(400);
+
+    std::vector<cv::KeyPoint> keypoints;
+    detector->detect(inputImage, keypoints);
+
+    cv::Mat keyPointsImage;
+
+    cv::drawKeypoints(inputImage, keypoints, keyPointsImage);
+
+    showImage(keyPointsImage);
+}
+
+void showImage(std::string &imageName) {
     cv::Mat testing = cv::imread(imageName);
 
     if (!testing.data) {
         std::cerr << "unable to read image" << std::endl;
     }
 
-    cv::namedWindow("testing open cv", cv::WINDOW_AUTOSIZE);
-    cv::imshow("image name", testing);
+    showImage(testing);
+}
+
+void showImage(cv::Mat &image) {
+    cv::namedWindow("image", cv::WINDOW_AUTOSIZE);
+    cv::imshow("image name", image);
 
     cv::waitKey(0);
 }
